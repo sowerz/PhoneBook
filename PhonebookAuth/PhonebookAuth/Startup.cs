dfa8f7a1c023fa308649575a5a5287451ba57e0a
sowerz <54650392+sowerz@users.noreply.github.com>
2019-12-12 22:15:23 +0700

    Added Phone Book to repository

---
+++ PhonebookAuth/PhonebookAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhonebookAuth.Startup))]
+namespace PhonebookAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
